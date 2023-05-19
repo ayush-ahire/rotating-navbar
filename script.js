0a8d736d9036f61355b36545b162e7cda85989cc
ayush-ahire <ayushgithub8@gmail.com>
2023-05-19 20:17:25 +0530

    rotating navbar

---
+++ script.js
@@ -0,0 +1,10 @@
+const open = document.getElementById("open")
+const container = document.querySelector(".container")
+const close = document.getElementById("close")
+
+open.addEventListener('click',()=>{
+    container.classList.add("show-nav")
+})
+close.addEventListener('click',()=>{
+    container.classList.remove("show-nav")
+})
